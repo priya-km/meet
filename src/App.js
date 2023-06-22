e2d5ba44dc8d6f2fad23eb2a60b2c6af2364554b
priya-km <priya.k.mah@gmail.com>
2023-06-22 16:10:32 -0400

    Moved number of events up

--- src/App.js
+++ src/App.js
@@ -84,13 +84,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CitySearch
-          locations={this.state.locations}
-          updateEvents={this.updateEvents}
-        />
         <NumberOfEvents
           selectedCity={this.state.selectedCity}
           query={this.state.eventCount}
+          updateEvents={this.updateEvents}
+        />
+        <CitySearch
+          locations={this.state.locations}
           updateEvents={this.updateEvents}
         />
         <EventList events={this.state.events} />
